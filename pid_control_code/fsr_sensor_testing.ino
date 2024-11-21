c947dbb727993d271c59b9a80f2cac3591b1a5df
Mihir Kelkar <mihkel@umich.edu>
2024-11-20 20:28:30 -0500

    bug fix?

--- pid_control_code/fsr_sensor_testing.ino
+++ pid_control_code/fsr_sensor_testing.ino
@@ -1,12 +1,12 @@
 const int FSR_PIN = A0; // Analog pin connected to FSR
-#define MOVING_AVERAGE_BUFFER 8
+#define MOVING_AVERAGE_BUFFER 15
 #define MAX_READING 1023 //implies maximum pressure applied to sensor
 #define MIN_READING 0 //implies minimum pressure applied to sensor
 
 struct MovingAverage{
-  double starting_value = (MAC_READING + MIN_READING) / 2
-  uint8_t arr[MOVING_AVERAGE_BUFFER] = { starting_value };
-  uint8_t counter = 0;
+  double starting_value = (MAX_READING + MIN_READING) / 2;
+  double arr[MOVING_AVERAGE_BUFFER] = { starting_value };
+  int counter = 0;
   double sum = starting_value * MOVING_AVERAGE_BUFFER;
 
   //Returns one data point and updates average
@@ -15,20 +15,21 @@ struct MovingAverage{
     sum -= arr[counter];
 
     // Read in newest value and overwrite oldest
-    arr[counter] = analogRead(FSR_PIN);
+    // arr[counter] = analogRead(FSR_PIN);
+    arr[counter] = new_value;
 
     // Update sum to reflect new value
     sum += arr[counter];
 
     //Increment counter and do modulo to make sure it wraps around
-    counter = (counter + 1) % MOVING_AVERAGE_BUFFER
+    counter = ((counter + 1) % MOVING_AVERAGE_BUFFER);
 
     //return average
-    Serial.println(sum / MOVING_AVERAGE_BUFFER);
-    return sum / (double(MOVING_AVERAGE_BUFFER));
+    // Serial.println(sum / MOVING_AVERAGE_BUFFER);
+    return (sum / (double(MOVING_AVERAGE_BUFFER)));
 
   }// updateAvg
-}
+};
 
 MovingAverage avg;
 
@@ -51,5 +52,5 @@ void loop() {
   Serial.println(balancedfsrReading);
 
   // Add a delay to make readings more readable
-  delay(500); // 500 ms delay
+  delay(50); // 500 ms delay
 }
